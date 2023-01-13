c0bbde93971f46357f1a4b041c73f0284fca9222
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-13 03:13:51 -0300

    added more from streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,7 +1,7 @@
 // process.stdin
 //   .pipe(process.stdout);
 
-import { Readable } from 'node:stream';
+import { Readable, Writable, Transform } from 'node:stream';
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -21,5 +21,22 @@ class OneToHundredStream extends Readable {
   }
 }
 
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+
+    callback()
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
 new OneToHundredStream()
-  .pipe(process.stdout);
+  .pipe(new InverseNumberStream)
+  .pipe(new MultiplyByTenStream);

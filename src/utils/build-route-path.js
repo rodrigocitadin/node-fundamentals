db6925280fdde58bec7a1ecaf6277bd6d4f23494
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-23 18:39:18 -0300

    using regex to extract query params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,5 +2,5 @@ export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-zA-Z0-9\-_]+)');
 
-  return new RegExp(`^${pathWithParams}`);
+  return new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 }

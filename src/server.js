5e5a6787c43f59b3a1464fec51dd7e56ad1a0940
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-23 18:09:30 -0300

    using regex to route params

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url;
-  })
+    return route.method === method && route.path.test(url);
+  });
 
   if (route) {
     return route.handler(req, res);
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,6 @@
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-zA-Z0-9\-_])');
+
+  return new RegExp(`^${pathWithParams}`);
+}
